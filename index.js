d38a685e563c5302bc47834e334e0933680587d8
Brendan Jennings <jbrendan70@outlook.com>
2021-07-25 14:01:01 +0100

    ✨ 🎨  Domain parser logic written and cleaned

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const proxy = require('http-proxy');
 const https = require('https');
 const config = require('../loader.js');
 
-//domain parser
-const domainParser = (req) => {
-
+//domain parser logic
+const domainParser = async (req) => {
+    return new Promise((resolve, reject) => {
+        const domainArray = String(req.headers.host).split(':')[0].split('.');
+        resolve(String(`${domainArray[1]}.${domainArray[2]}.${config.domain_options.host_domain}:${domainArray[0]}`));
+    });
 };
 
-https.createServer(config.ssl, (req, res) => {
+https.createServer(config.ssl, async (req, res) => {
     req.on('error', (err) => {
         console.error(String(err.message));
     });
-    setTimeout(function () {
+    setTimeout(async function () {
         proxy.web(req, res, {
-            target: domainParser(req)
+            target: await domainParser(req)
         });
     }, 5);
 }).listen(loader.port);
