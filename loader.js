cefb592b9df8457399ef5c184d701ac1b5bc1d94
Brendan Jennings <jbrendan70@outlook.com>
2021-07-25 13:42:02 +0100

    ✨ 🔧  Better config system allowing the merging of non-source managed configs

--- loader.js
+++ loader.js
@@ -1,5 +1,7 @@
-// read config file and convert to object
+const _ = require("lodash");
+
 try {
-    const configOriginal = JSON.parse(require("fs").readFileSync('./config.json','utf8'));
-    module.exports = configOriginal;
+    const configOverrides = JSON.parse(require("fs").readFileSync('./config.overrides.json','utf8')),
+        configOriginal = JSON.parse(require("fs").readFileSync('./config.defaults.json','utf8'));
+    module.exports = _.merge(configOriginal, configOverrides);
 } catch (err) { throw new Error("Failed to read or parse config, error given by JSON or fs:\n    "+err); }
